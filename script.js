069b909188cbe68532a03b6cf095d79dc0883a06
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-05-11 17:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,76 +1,62 @@
 //your JS code here. If required.
-let books = []
 
-function addBook(title,isbn,author){
+ const form = document.getElementById('book-form');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const isbnInput = document.getElementById('isbn');
+const bookList = document.getElementById('book-list');
 
-  books.push({title,isbn,author})
-  updateTable({title,isbn,author})
-  saveToLocalStorage()
+form.addEventListener('submit', addBook);
+bookList.addEventListener('click', deleteBook);
+
+function addBook(e) {
+	e.preventDefault();
+	const title = titleInput.value;
+	const author = authorInput.value;
+	const isbn = isbnInput.value;
+
+	if (title === '' || author === '' || isbn === '') {
+		alert('Please fill in all fields');
+		return;
+	}
+
+	const book = {
+		title,
+		author,
+		isbn
+	};
+
+	addBookToList(book);
+	clearFields();
 }
 
-function deleteBook(el) {
-  if(el.classList.contains('delete')) {
-    el.parentElement.parentElement.remove();
-  }
+function addBookToList(book) {
+	const row = document.createElement('tr');
+	row.innerHTML = `
+		<td>${book.title}</td>
+		<td>${book.author}</td>
+		<td>${book.isbn}</td>
+		<td><button class="btn btn-danger btn-sm delete">X</button></td>
+	`;
+	bookList.appendChild(row);
 }
 
-document.querySelector('#book-list').addEventListener('click', (e) => {
-  // Remove book from UI
-  e.preventDefault();
-
-  deleteBook(e.target);
-
-  // Remove book from store
-  const isbn = e.target.parentElement.previousElementSibling.textContent;
-
-  books = books.filter(book=>book.isbn!==isbn)
-  saveToLocalStorage()
-});
-
-
-function updateTable(book){
-  const list = document.querySelector('#book-list');
-  
-  const row = document.createElement('tr');
-
-  row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-  `;
-
-  list.appendChild(row);
-
+function clearFields() {
+	titleInput.value = '';
+	authorInput.value = '';
+	isbnInput.value = '';
 }
 
-function saveToLocalStorage(){
-
-  localStorage.setItem('books1', JSON.stringify(books));
+function deleteBook(e) {
+	if (e.target.classList.contains('delete')) {
+		if (confirm('Are you sure?')) {
+			const row = e.target.parentElement.parentElement;
+			row.remove();
+		}
+	}
 }
-
-function getDataFromLocalStorage(){
-  if(localStorage.getItem('books1') === null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem('books1'));
-
-    books.forEach(book=> updateTable(book))
-  }
-}
+ 
 
 
-document.querySelector('#submit').addEventListener("click",(e)=>{
 
-    e.preventDefault();
-  
-    // Get form values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
 
-    addBook(title,isbn,author)
-
-})
-
-getDataFromLocalStorage()
